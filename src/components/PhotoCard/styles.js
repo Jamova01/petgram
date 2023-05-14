ec5016a18ead4e9d7091f96596f459bedab230e2
Jamova01 <jorgemova01@gmail.com>
2023-05-13 19:06:39 -0500

    [UPDATE]: Updating button styles

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -26,5 +26,10 @@ export const Img = styled.img`
   width: 100%;
 `
 export const Button = styled.button`
+  display: flex;
+  align-items: center;
   padding-top: 8px;
+  & svg {
+    margin: 4px;
+  }
 `
