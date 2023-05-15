932193db0c97523568133002e4903178b43d964d
Jamova01 <jorgemova01@gmail.com>
2023-05-15 07:17:47 -0500

    [UPDATE]: Container

---
+++ src/container/ListOfPhotoCardsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { withPhotos } from '../hoc/withPhotos';
+import { ListOfPhotoCards } from '../components/ListOfPhotoCards';
+
+const ListOfPhotoCardsContainer = ({ data }) => {
+  return <ListOfPhotoCards data={data} />
+}
+export default withPhotos(ListOfPhotoCardsContainer)
---
+++ src/container/PhotoCardWithQuery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { withPhoto } from '../hoc/withPhoto'
+import { PhotoCard } from '../components/PhotoCard'
+
+const PhotoCardWithQuery = ({ id, likes, src }) => {
+  return <PhotoCard id={id} likes={likes} src={src} />;
+};
+
+export default withPhoto(PhotoCardWithQuery);
