c88bd1cebb38c5aaecb36b3037c2b461cfb4f6f1
Jamova01 <jorgemova01@gmail.com>
2023-05-15 07:12:11 -0500

    [UPDATE]: Adding the data prop to the component.

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+export const ListOfPhotoCards = ({ data }) => {
   return (
     <ul>
-      {
-        [1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)
-      }
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
+      ))}
     </ul>
   )
 }
