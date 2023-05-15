2e30441bb6d0a5c1053e37ece72abe3f5a3ab475
Jamova01 <jorgemova01@gmail.com>
2023-05-15 07:17:19 -0500

    [UPDATE]: higher-order component

---
+++ src/hoc/withPhoto.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+export const withPhoto = (WrappedComponent) => {
+  return ({ id }) => {
+    const GET_SINGLE_PHOTO = gql`
+      query GetPhoto($id: ID!) {
+        photo(id: $id) {
+          id
+          likes
+          src
+        }
+      }
+    `
+
+    const { loading, error, data } = useQuery(GET_SINGLE_PHOTO, {
+      variables: { id },
+    })
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error: {error.message}</p>
+
+    const { photo = {} } = data
+
+    return <WrappedComponent {...photo} />
+  }
+}
---
+++ src/hoc/withPhotos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+export const withPhotos = (WrappedComponent) => {
+  return ({ categoryId }) => {
+    const GET_PHOTOS = gql`
+      query GetPhotos($categoryId: ID) {
+        photos(categoryId: $categoryId) {
+          id
+          categoryId
+          src
+          likes
+          userId
+          liked
+        }
+      }
+    `
+
+    const { loading, error, data } = useQuery(GET_PHOTOS, {
+      variables: { categoryId }
+    })
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error: {error.message}</p>
+
+    return <WrappedComponent data={data} />
+  }
+}
