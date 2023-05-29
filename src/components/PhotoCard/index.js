1b57822cfb781fed9c0fa29ec09ef7f18be904ec
Jamova01 <jorgemova01@gmail.com>
2023-05-29 12:11:28 -0500

    [UPDATE]: Implementing ToggleLikeMutation component

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,10 +1,9 @@
 import React, { Fragment } from 'react'
-import { Button, Img, ImgWrapper, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Img, ImgWrapper, Article } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { Link } from 'react-router-dom'
-
+import ToggleLikeMutation from '../../container/ToggleLikeMutation'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -12,20 +11,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [liked, setLiked] = useLocalStorage(key, false)
   const [show, ref] = useNearScreen()
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
-
   return (
     <Article ref={ref}>
       {
         show && <Fragment>
-          <Link to={`/photos/${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
           </Link>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' /> {likes} likes!
-          </Button>
+          <ToggleLikeMutation
+            photoId={id}
+            likes={likes}
+            liked={liked}
+            setLiked={setLiked}
+          />
         </Fragment>
       }
 
