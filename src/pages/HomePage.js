f20a79e022194494a42defda9cc8b92267c649bc
Jamova01 <jorgemova01@gmail.com>
2023-05-29 11:52:59 -0500

    [UPDATE]: Providing the category ID.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import ListOfPhotoCardsContainer from '../container/ListOfPhotoCardsContainer'
+import { useParams } from 'react-router-dom'
 
 export const HomePage = () => {
+  const { id } = useParams()
   return (
     <Fragment>
       <ListOfCategories />
-      <ListOfPhotoCardsContainer />
+      <ListOfPhotoCardsContainer categoryId={id} />
     </Fragment>
   )
 }
