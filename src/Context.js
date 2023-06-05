c607d94e484774e741ee972515e4d949ea1e6416
Jamova01 <jorgemova01@gmail.com>
2023-06-04 23:26:58 -0500

    [CREATE]: Context

---
+++ src/Context.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react'
+const Context = createContext(null)
+
+const Provider = (props) => {
+  const [isAuth, setIsAuth] = useState(() => {
+    return window.sessionStorage.getItem('token')
+  })
+
+  const activateAuth = (token) => {
+    setIsAuth(true)
+    window.sessionStorage.setItem('token', token)
+  }
+
+  return (
+    <Context.Provider value={{ isAuth, activateAuth }}>
+      {props.children}
+    </Context.Provider>
+  )
+}
+
+export {
+  Provider,
+  Context
+}
