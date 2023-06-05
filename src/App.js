85925a440e08363afe8a6b3c8021a3d8ef17cafd
Jamova01 <jorgemova01@gmail.com>
2023-06-04 23:30:39 -0500

    [UPDATE]: Protected routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { GlobalStyle } from './styles/GlobalStyles'
+import React, { useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, } from 'react-router-dom'
+
+// Components
 import { Logo } from './components/Logo'
-import { HomePage } from './pages/HomePage';
-import { PhotoDetail } from './pages/PhotoDetail';
+import { NavBar } from './components/NavBar'
+
+// Pages
+import { HomePage } from './pages/HomePage'
+import { UserPage } from './pages/UserPage'
+import { FavoritesPage } from './pages/FavoritesPage'
+import { PhotoDetailPage } from './pages/PhotoDetail'
+import { NotRegisteredUser } from './pages/NotRegisteredUserPage'
+import { Context } from './Context'
+
+// Styles
+import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
 
   return (
     <div>
       <GlobalStyle />
-      <Logo />
       <Router>
+        <Link to='/'>
+          <Logo />
+        </Link>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/photos/:id' element={<PhotoDetail />} />
+          <Route path="/pet/:id" element={<HomePage />} />
+          <Route path='/detail/:id' element={<PhotoDetailPage />} />
+          <Route path="/favorites" element={isAuth ? <FavoritesPage /> : <Navigate to='/login' />} />
+          <Route path="/user" element={isAuth ? <UserPage /> : <Navigate to='/login' />} />
+          <Route path="/login" element={<NotRegisteredUser />} />
         </Routes>
+        <NavBar />
       </Router>
     </div>
   )
