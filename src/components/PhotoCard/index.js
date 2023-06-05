39a35a0b6b535406210e2867c3ead5f51551f9a7
Jamova01 <jorgemova01@gmail.com>
2023-06-05 01:44:27 -0500

    [UPDATE]: Removing useLocalStorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react'
 import { Img, ImgWrapper, Article } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
+// import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { Link } from 'react-router-dom'
 import ToggleLikeMutation from '../../container/ToggleLikeMutation'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, ref] = useNearScreen()
 
   return (
@@ -24,7 +22,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             photoId={id}
             likes={likes}
             liked={liked}
-            setLiked={setLiked}
           />
         </Fragment>
       }
