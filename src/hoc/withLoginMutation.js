f188393cd66b1169bc6d237a38e49e5f0d07f71e
Jamova01 <jorgemova01@gmail.com>
2023-06-04 23:22:15 -0500

    [CREATER]: HOC Login Mutation

---
+++ src/hoc/withLoginMutation.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { useInputValue } from '../hooks/useInputValue';
+import { Context } from '../Context';
+
+const LOGIN = gql`
+  mutation login($input: UserCredentials!){
+    login(input: $input)
+  }
+`
+export const withLoginMutation = (WrappedComponent) => {
+  return () => {
+    const email = useInputValue('')
+    const password = useInputValue('')
+    const { activateAuth } = useContext(Context)
+    const [mutation, { loading, error }] = useMutation(LOGIN)
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      mutation({ variables: { input: { email: email.value, password: password.value } } }).then(({ data }) => {
+        const { login } = data
+        console.log(login);
+        activateAuth(login)
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
+
+    return (
+      <WrappedComponent
+        mutation={mutation}
+        onSubmit={handleSubmit}
+        title='Iniciar sesión'
+        disabled={loading}
+        email={email}
+        password={password} />
+    )
+  }
+}
