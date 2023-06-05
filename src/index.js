962400db965cebdb0541edec9dd9b08a1c3e06e1
Jamova01 <jorgemova01@gmail.com>
2023-06-04 23:33:01 -0500

    [CREATE]: Implementing apollo & Context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+
+// Router DOM
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+
+// Apollo
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// Context
+import { Provider } from './Context';
+
+import { App } from './App'
 
 const client = new ApolloClient({
   uri: 'https://petgram-server-jamova01.vercel.app/graphql',
@@ -12,7 +20,10 @@ const container = document.getElementById('app')
 const root = createRoot(container)
 
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Provider>
+
 );
