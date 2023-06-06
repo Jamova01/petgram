9e014bdacee838c5e39c47d2cf2824d2431e1069
Jamova01 <jorgemova01@gmail.com>
2023-06-05 23:52:05 -0500

    [UPDATE]: Implementing Sign out functionality

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { Fragment, useContext } from 'react'
+import { SubmitButton } from '../components/SubmitButton'
+import { Context } from '../Context'
 
 export const UserPage = () => {
-  return(
+  const { removeAuth } = useContext(Context)
+  return <Fragment>
     <h1>User</h1>
-  )
+    <SubmitButton onClick={removeAuth}>Cerrar sesión</SubmitButton>
+  </Fragment>
 }
