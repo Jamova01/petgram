8d7bf6bba0d7727c6854919fa1f6b329c51f3ecd
Jamova01 <jorgemova01@gmail.com>
2023-06-05 23:45:07 -0500

    [UPDATE]: Implementing SubmitButton component

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,28 +1,25 @@
 import React, { Fragment } from 'react'
-import { Form, Input, Button, Title } from './styles'
+import { Form, Input, Title } from './styles'
+import { SubmitButton } from '../SubmitButton'
 
 export const UserForm = ({ onSubmit, title, disabled, email, password }) => {
 
-  return (
-    <Fragment>
-      <Title>{title}</Title>
-      <Form onSubmit={onSubmit}>
-        <Input
-          placeholder='Email'
-          disabled={disabled}
-          {...email} />
-        <Input
-          placeholder='Password'
-          type='password'
-          disabled={disabled}
-          {...password} />
-        <Button
-          disabled={disabled}>
-          {title}
-        </Button>
-      </Form>
-
-    </Fragment>
-
-  )
+  return <Fragment>
+    <Title>{title}</Title>
+    <Form onSubmit={onSubmit}>
+      <Input
+        placeholder='Email'
+        disabled={disabled}
+        {...email} />
+      <Input
+        placeholder='Password'
+        type='password'
+        disabled={disabled}
+        {...password} />
+      <SubmitButton
+        disabled={disabled}>
+        {title}
+      </SubmitButton>
+    </Form>
+  </Fragment>
 }
