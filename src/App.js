a408a75e194ec0324af26dafcb36fc8bc90d6cfa
Jamova01 <jorgemova01@gmail.com>
2023-06-07 00:20:52 -0500

    [UPDATE]: Implementing Suspense & React.lazy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useContext } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, Navigate, } from 'react-router-dom'
+import React, { Suspense, lazy, useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
+
+// Context
+import { Context } from './Context'
 
 // Components
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 
-// Pages
-import { HomePage } from './pages/HomePage'
-import { UserPage } from './pages/UserPage'
-import { FavoritesPage } from './pages/FavoritesPage'
-import { PhotoDetailPage } from './pages/PhotoDetail'
-import { NotRegisteredUser } from './pages/NotRegisteredUserPage'
-import { Context } from './Context'
-
 // Styles
 import { GlobalStyle } from './styles/GlobalStyles'
 import { NotFoundPage } from './pages/NotFoundPage'
+
+// Pages
+const HomePage = lazy(() => import('./pages/HomePage'))
+const UserPage = lazy(() => import('./pages/UserPage'))
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage'))
+const PhotoDetailPage = lazy(() => import('./pages/PhotoDetail'))
+const NotRegisteredUser = lazy(() => import('./pages/NotRegisteredUserPage'))
 
 export const App = () => {
   const { isAuth } = useContext(Context)
@@ -27,15 +29,17 @@ export const App = () => {
         <Link to='/'>
           <Logo />
         </Link>
-        <Routes>
-          <Route path='*' element={<NotFoundPage />} />
-          <Route path='/' element={<HomePage />} />
-          <Route path="/pet/:id" element={<HomePage />} />
-          <Route path="/login" element={<NotRegisteredUser />} />
-          <Route path='/detail/:id' element={<PhotoDetailPage />} />
-          <Route path="/user" element={isAuth ? <UserPage /> : <Navigate to='/login' />} />
-          <Route path="/favorites" element={isAuth ? <FavoritesPage /> : <Navigate to='/login' />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='*' element={<NotFoundPage />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/pet/:id' element={<HomePage />} />
+            <Route path='/login' element={<NotRegisteredUser />} />
+            <Route path='/detail/:id' element={<PhotoDetailPage />} />
+            <Route path='/user' element={isAuth ? <UserPage /> : <Navigate to='/login' />} />
+            <Route path='/favorites' element={isAuth ? <FavoritesPage /> : <Navigate to='/login' />} />
+          </Routes>
+        </Suspense>
         <NavBar />
       </Router>
     </div>
